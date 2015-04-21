b9c4d63a28929a492671c7c1349c1c82fb9181e8
Your Name <bengteh@itk-d651>
2015-04-21 04:55:54 +0200

    added elvator project

---
+++ cpp/Elevator_Project/src/Elevator.h
@@ -0,0 +1,20 @@
+/*
+ * elevator.h
+ *
+ *  Created on: Feb 11, 2015
+ *      Author: bengteh
+ */
+
+#ifndef EXERCISE5_COMEDIDRV_SRC_ELEVATOR_H_
+#define EXERCISE5_COMEDIDRV_SRC_ELEVATOR_H_
+
+class Elevator
+{
+public:
+	Elevator();
+	~Elevator();
+private:
+
+};
+#endif /* EXERCISE5_COMEDIDRV_SRC_ELEVATOR_H_ */
+
